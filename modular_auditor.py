3ff2634552b0f64d6ec54e5f526a1fbc552e2d60
2601690 <2601690@sit.singaporetech.edu.sg>
2026-09-23 17:46:59 +0800

    add process delivery function

--- modular_auditor.py
+++ modular_auditor.py
@@ -11,3 +11,7 @@
 
 # generate report(total_units, failed_attempts) a dedicated function to print the final summarry
 # input final inventory and failed attempts count | output the final report
+
+# process delivery that returns updated inventory
+def process_delivery(current_total, new_value):
+    return current_total + new_value
