7ef854c66b6d6c5d03ce770d151c4675cd74613a
2601690 <2601690@sit.singaporetech.edu.sg>
2026-09-23 17:30:32 +0800

    auditor planning docs

---
+++ modular_auditor.py
@@ -0,0 +1,13 @@
+# Week 3 - Modular Auditor
+
+# get_valid_input() handles the prompt, input validation and return a valid integer or a "quit" signal
+# input none | output valid stock quantity, "quit" or invalid status
+
+# process_delivery(current_total, new_value) calculates the new total and returns it
+# input current inventory and new value | output updated inventory
+
+# calculate_tax(amount) a new requirement, this function takes a delivery amount and returns the tax (10% of that specific delivery)
+# input delivery amount | output 10% tax for that delivery
+
+# generate report(total_units, failed_attempts) a dedicated function to print the final summarry
+# input final inventory and failed attempts count | output the final report
